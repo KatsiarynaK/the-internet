f9652d0d128e6f9d1bc0634382b27d70d1498b31
KatsiarynaK <kate278@tut.by>
2017-05-09 00:09:25 +0200

    dependencies added

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -1,7 +1,18 @@
 
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class LoginTest {
 	
-
+	void testLogin(){
+		//1. open the browser
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "D://Soft_installed//eclipse//workspace//the-internet//geckodriver-v0.15.0-win64//geckodriver.exe");
+		driver =new FirefoxDriver();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
