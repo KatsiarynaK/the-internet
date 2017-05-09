22c8cb304904f902b9fb1cbaa752d6c3ccbf30b7
KatsiarynaK <kate278@tut.by>
2017-05-09 23:03:43 +0200

    type credentials

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -1,4 +1,7 @@
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -9,7 +12,7 @@ import org.openqa.selenium.By;
 
 public class LoginTest {
 	
-	static void testLogin(){
+	static void testLogin() throws AWTException, InterruptedException{
 		//1. open the browser
 		WebDriver driver;
 		System.setProperty("webdriver.gecko.driver", "D://Soft_installed//eclipse//workspace//the-internet//geckodriver-v0.15.0-win64//geckodriver.exe");
@@ -17,18 +20,51 @@ public class LoginTest {
 		driver.get("https://the-internet.herokuapp.com/");
 		WebElement loginPage = driver.findElement(By.xpath(".//*[@id='content']/ul/li[2]/a"));
 		loginPage.click();
+		Thread.sleep(5000);
 		//go to pop up window
 		String parentWindowHandler = driver.getWindowHandle();
 		String childWindowHandler = null;
 		Set<String> handles = driver.getWindowHandles();
 		Iterator<String> iter = handles.iterator();
+		Thread.sleep(5000);
 		while (iter.hasNext()){
 			childWindowHandler = iter.next();
 		}
 		driver.switchTo().window(childWindowHandler);
+		System.out.println(parentWindowHandler);
+		System.out.println(childWindowHandler);
+		//type in pop up window
+		Thread.sleep(5000);
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_A);
+		r.keyRelease(KeyEvent.VK_A);
+		r.keyPress(KeyEvent.VK_D);        
+		r.keyRelease(KeyEvent.VK_D);
+		r.keyPress(KeyEvent.VK_M);
+		r.keyRelease(KeyEvent.VK_M);
+		r.keyPress(KeyEvent.VK_I);
+		r.keyPress(KeyEvent.VK_I);
+		r.keyPress(KeyEvent.VK_N);
+		r.keyPress(KeyEvent.VK_N);
+		r.delay(5000);
+		r.keyPress(KeyEvent.VK_TAB);
+		r.keyRelease(KeyEvent.VK_TAB);
+		r.keyPress(KeyEvent.VK_A);
+		r.keyRelease(KeyEvent.VK_A);
+		r.keyPress(KeyEvent.VK_D);        
+		r.keyRelease(KeyEvent.VK_D);
+		r.keyPress(KeyEvent.VK_M);
+		r.keyRelease(KeyEvent.VK_M);
+		r.keyPress(KeyEvent.VK_I);
+		r.keyPress(KeyEvent.VK_I);
+		r.keyPress(KeyEvent.VK_N);
+		r.keyPress(KeyEvent.VK_N);
+		r.delay(5000);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AWTException, InterruptedException {
 		// TODO Auto-generated method stub
 		testLogin();
 	}
