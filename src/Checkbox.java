fed9e6644d4298b43d4231df332affa7665232fa
KatsiarynaK <kate278@tut.by>
2017-05-25 10:43:05 +0200

    Changed all Thread.sleep to WebdriverWait

--- src/Checkbox.java
+++ src/Checkbox.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import junit.framework.Assert;
@@ -10,18 +11,21 @@ import junit.framework.Assert;
 public class Checkbox {
 
 	static WebDriver driver ;
-	public static void main(String[] args) throws InterruptedException {
+	static WebDriverWait wait;
+	
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.gecko.driver", "D://Soft_installed//eclipse//workspace//the-internet//geckodriver-v0.15.0-win64//geckodriver.exe");
 		driver = new FirefoxDriver();
+		wait = new WebDriverWait(driver,5);
 		driver.get("https://the-internet.herokuapp.com/");
 		driver.manage().window().maximize();
-		Thread.sleep(5000);
 		// CSS locator!!!
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#content > ul li:nth-child(5) a")));
 		WebElement link = driver.findElement(By.cssSelector("div#content > ul li:nth-child(5) a"));
 		link.click();
-		//check whether checkbox is already displayed
-		WebDriverWait wait = new WebDriverWait(driver,5);
+		//check whether check-box is already displayed
+		
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("checkboxes")));
 		if (isDisplayed(By.id("checkboxes"))){
 			System.out.println("Checkboxes are displayed");
@@ -35,7 +39,7 @@ public class Checkbox {
 	public static boolean isDisplayed (By locator){
 		try{
 			return driver.findElement(locator).isDisplayed();
-		}catch (org.openqa.selenium.NoSuchElementException exeption){
+		}catch (org.openqa.selenium.NoSuchElementException exception){
 			return false;
 		}
 	}
